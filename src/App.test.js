463e2c3c349024aa4247e5d39df8908bcbe08512
Jen Stewart <jenstewart2404@gmail.com>
2021-09-27 16:27:05 +0100

    Project setup tidying up public and src folders

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders weather app text', () => {
+	render(<App />);
+	const linkElement = screen.getByText(/Weather App/i);
+	expect(linkElement).toBeInTheDocument();
 });
