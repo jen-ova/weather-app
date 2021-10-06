e92e5545415e0df291008f6bfa18f2a5470b6cb7
Jen Stewart <jenstewart2404@gmail.com>
2021-10-06 14:12:01 +0100

    Rendered forecast summaries list with summary components in app and created basic css to flex the summary cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
-
+import ForecastSummaries from "./ForecastSummaries";
 import "../styles/app.css";
 
-const App = ({ location }) => {
-  const { city, country } = location;
-  return <LocationDetails city={city} country={country} />;
+const App = ({ location, forecasts }) => {
+  return (
+    <div className="weather-app">
+      <LocationDetails city={location.city} country={location.country} />
+      <ForecastSummaries forecasts={forecasts} />
+    </div>
+  );
 };
 
 App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number.isRequired,
+      description: PropTypes.string.isRequired,
+      icon: PropTypes.string.isRequired,
+      temperature: PropTypes.shape({
+        min: PropTypes.number,
+        max: PropTypes.number,
+      }).isRequired,
+    })
+  ).isRequired,
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
