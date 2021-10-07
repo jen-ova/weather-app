8b0ef547d6cec7451e0501c81f03c5572c857222
Jen Stewart <jenstewart2404@gmail.com>
2021-10-07 15:35:06 +0100

    created forecast summaries tests for snapshot and instances

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -6,7 +6,7 @@ const ForecastSummary = (props) => {
   const { date, description, icon, temperature } = props;
 
   return (
-    <div className="forecast-summary">
+    <div className="forecast-summary" data-testid="forecast-summary">
       <div className="forecast-summary__date">{date}</div>
       <div className="forecast-summary__description">{description}</div>
       <div className="forecast-summary__icon">
---
+++ src/tests/components/ForecastSummaries.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummaries from "../../components/ForecastSummaries";
+
+describe("ForecastSummaries", () => {
+  const validProps = [
+    {
+      date: 1111111,
+      description: "stubDescription1",
+      icon: "stubIcon1",
+      temperature: {
+        min: 12,
+        max: 24,
+      },
+    },
+    {
+      date: 2222222,
+      description: "stubDescription2",
+      icon: "stubIcon2",
+      temperature: {
+        min: 7,
+        max: 14,
+      },
+    },
+  ];
+
+  it("renders correctly", () => {
+    const { asFragment } = render(<ForecastSummaries forecasts={validProps} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders the correct number of ForecastSummary instances", () => {
+    const { getAllByTestId } = render(
+      <ForecastSummaries forecasts={validProps} />
+    );
+    expect(getAllByTestId("forecast-summary")).toHaveLength(2);
+  });
+});
