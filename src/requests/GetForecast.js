db443757ac6997f5db7e893ddeae71b0fc79f261
Jen Stewart <jenstewart2404@gmail.com>
2021-10-10 16:28:16 +0100

    added error handling to 404 and 500

--- src/requests/GetForecast.js
+++ src/requests/GetForecast.js
@@ -4,7 +4,8 @@ const getForecast = (
   searchText,
   setSelectedDate,
   setForecasts,
-  setLocation
+  setLocation,
+  setErrorMessage
 ) => {
   let endpoint = "https://mcr-codes-weather-app.herokuapp.com/forecast";
 
@@ -12,11 +13,24 @@ const getForecast = (
     endpoint += `?city=${searchText}`;
   }
 
-  axios.get(endpoint).then((response) => {
-    setSelectedDate(response.data.forecasts[0].date);
-    setForecasts(response.data.forecasts);
-    setLocation(response.data.location);
-  });
+  axios
+    .get(endpoint)
+    .then((response) => {
+      setSelectedDate(response.data.forecasts[0].date);
+      setForecasts(response.data.forecasts);
+      setLocation(response.data.location);
+    })
+    .catch((error) => {
+      const { status } = error.response;
+      if (status === 404) {
+        setErrorMessage("City is not found");
+        console.error("City is not found", error);
+      }
+      if (status === 500) {
+        setErrorMessage("Server error");
+        console.error("Server error", error);
+      }
+    });
 };
 
 export default getForecast;
